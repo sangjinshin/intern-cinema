f501498381b90723b8ba0034efe4cabaa49a7196
Sangjin Shin <sangjin.shin@sailpoint.com>
2019-05-31 13:30:30 -0500

    Add injector

--- src/main/java/com/sailpoint/interncinema/conf/CinemaApplication.java
+++ src/main/java/com/sailpoint/interncinema/conf/CinemaApplication.java
@@ -1,10 +1,15 @@
 package com.sailpoint.interncinema.conf;
 
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.sailpoint.interncinema.resource.MovieResource;
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,22 +19,28 @@ public class CinemaApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
 	private Set<Class<?>> classes = new HashSet<Class<?>>();
 
-	public CinemaApplication() {
+	public CinemaApplication(@Context ServletContext servletContext) {
 	}
 
 	@Override
 	public Set<Class<?>> getClasses() {
-		CorsFilter corsFilter = new CorsFilter();
-		corsFilter.getAllowedOrigins().add("*");
-		corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
-		singletons.add(corsFilter);
+//		classes.add(MovieResource.class);
 
-		classes.add(MovieResource.class);
 		return classes;
 	}
 
 	@Override
 	public Set<Object> getSingletons() {
+
+		CorsFilter corsFilter = new CorsFilter();
+		corsFilter.getAllowedOrigins().add("*");
+		corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+		singletons.add(corsFilter);
+
+		Injector injector = Guice.createInjector(new MovieRepositoryModule());
+		MovieResource movieResource = injector.getInstance(MovieResource.class);
+		singletons.add(movieResource);
+
 		return singletons;
 	}
 }
