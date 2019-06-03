ebc1230275401648d866eababfd74f9acd071da1
Sangjin Shin <sangjin.shin@sailpoint.com>
2019-06-03 11:44:15 -0500

    Output result to response body

--- src/main/java/com/sailpoint/interncinema/resource/MovieResource.java
+++ src/main/java/com/sailpoint/interncinema/resource/MovieResource.java
@@ -2,7 +2,6 @@ package com.sailpoint.interncinema.resource;
 
 import com.google.inject.Inject;
 import com.sailpoint.interncinema.model.Movie;
-import com.sailpoint.interncinema.service.MovieRepository;
 import com.sailpoint.interncinema.service.Repository;
 
 import javax.ws.rs.*;
@@ -13,7 +12,7 @@ import java.util.List;
 @Path("movies")
 public class MovieResource {
 
-	Repository _repository;
+	Repository<Movie> _repository;
 
 	@Inject
 	public MovieResource(Repository repository) {
@@ -36,12 +35,9 @@ public class MovieResource {
 
 		Long id = _repository.add(movie);
 
-//		Movie output = _repository.get(id);
-//
-//		return Response.status(Response.Status.CREATED).entity(output).build();
-		_repository.get(id);
+		Movie output = _repository.get(id);
 
-		return Response.status(Response.Status.CREATED).build();
+		return Response.status(Response.Status.CREATED).entity(output).build();
 	}
 
 	@PUT
@@ -51,12 +47,9 @@ public class MovieResource {
 		if (movie == null)
 			return Response.status(Response.Status.BAD_REQUEST).build();
 
-//		Movie output = _repository.update(movie);
-//
-//		return Response.status(Response.Status.OK).entity(output).build();
+		Movie output = _repository.update(movie);
 
-		_repository.update(movie);
-		return Response.status(Response.Status.OK).build();
+		return Response.status(Response.Status.OK).entity(output).build();
 	}
 
 	@DELETE
@@ -66,13 +59,10 @@ public class MovieResource {
 		if (id == null)
 			return Response.status(Response.Status.BAD_REQUEST).build();
 
-//		Movie output = _repository.delete(_repository.get(id));
-//		if (output == null)
-//			return Response.status(Response.Status.NOT_FOUND).build();
-//
-//		return Response.status(Response.Status.OK).entity(output).build();
+		Movie output = _repository.delete(_repository.get(id));
+		if (output == null)
+			return Response.status(Response.Status.NOT_FOUND).build();
 
-		_repository.delete(_repository.get(id));
-		return Response.status(Response.Status.OK).build();
+		return Response.status(Response.Status.OK).entity(output).build();
 	}
 }
